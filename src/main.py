9e8135c81d909a1e3efae80b423b9a294180a99c
energizedNachos <88883885+energizedNachos@users.noreply.github.com>
2022-11-02 11:22:02 +0700

    [Update] Covariance Matrix Acquired

--- src/main.py
+++ src/main.py
@@ -7,7 +7,6 @@ from PIL import Image as im
 w, h = 250, 250
 
 imgfaces = []
-
 def read_training_data_set(nama):
     path = os.getcwd()
     path = path + "\\test\\training\\" + nama
@@ -25,10 +24,18 @@ def mean_phi(dataset):
     mean = mean / len(dataset)
     return mean
 
+def find_covariance(dataset):
+    mean = mean_phi(dataset)
+    cov = dataset[0] - mean
+    print(cov)
+    for i in range (1, len(dataset)):
+        np.concatenate((cov, dataset[i]-mean), axis=0)
+    return cov
+
 def __main__():
-    read_training_data_set("Robert Downey Jr")
-    mean = mean_phi(imgfaces)
-    data = im.fromarray(mean)
+    read_training_data_set("Chris Hemsworth")
+    cov = find_covariance(imgfaces)
+    data = im.fromarray(cov)
     data.show()
 
 if __name__ == '__main__':
