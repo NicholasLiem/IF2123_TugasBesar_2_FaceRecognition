f6cc0fab5031a276eed3fda973299906bf5f51ec
energizedNachos <88883885+energizedNachos@users.noreply.github.com>
2022-11-04 11:01:05 +0700

    Gatau ah, ga ngerti

--- src/main.py
+++ src/main.py
@@ -1,10 +1,10 @@
 import cv2
 import numpy as np
-import random
 import os
+import timeHandling as t
 from PIL import Image as im
 
-w, h = 250, 250
+w, h = 256, 256
 
 imgfaces = []
 def read_training_data_set(nama):
@@ -16,6 +16,7 @@ def read_training_data_set(nama):
             img = cv2.imread(path + '\\' + pic, cv2.IMREAD_GRAYSCALE)
             img = cv2.resize(img, (w, h))
             imgfaces.append(img)
+    print("Dataset has been loaded.")
 
 def mean_phi(dataset):
     mean = np.zeros((w, h))
@@ -24,19 +25,57 @@ def mean_phi(dataset):
     mean = mean / len(dataset)
     return mean
 
+# def find_covariance(dataset):
+#     mean = mean_phi(dataset)
+#     cov = dataset[1] - mean
+#     for i in range (1, len(dataset)):
+#         avg = dataset[i] - mean
+#         dataset[i] = avg
+#         cov = np.concatenate((cov, avg), axis=1)
+#     cov_mat = np.cov(dataset, rowvar = False)
+#     return cov_mat
+
 def find_covariance(dataset):
     mean = mean_phi(dataset)
     cov = dataset[0] - mean
-    print(cov)
     for i in range (1, len(dataset)):
-        np.concatenate((cov, dataset[i]-mean), axis=0)
-    return cov
+        cov = np.concatenate((cov, dataset[i] - mean), axis=1)
+    return np.matmul(cov, cov.T)
+
+def sort_eigen(cov):
+    eigenVal, eigenVec  = np.linalg.eig(cov)
+    idx = eigenVal.argsort()[::-1]
+    eigenVal = eigenVal[idx]
+    eigenVec = eigenVec[:,idx]
+    return eigenVec
 
 def __main__():
-    read_training_data_set("Chris Hemsworth")
-    cov = find_covariance(imgfaces)
-    data = im.fromarray(cov)
+    read_training_data_set("Robert Downey Jr")
+    t.tic()
+    eigens = sort_eigen(find_covariance(imgfaces))
+    print((eigens.T)[0].shape)
+    res = np.matmul(eigens, imgfaces[0]-mean_phi(imgfaces))
+    data = im.fromarray(res)
     data.show()
+    
+    # path = os.getcwd()
+    # path = path + "\\test\\training\\" + "testing.jpg"
+    # newImg = cv2.imread(path, cv2.IMREAD_GRAYSCALE)
+    # newImg = cv2.resize(newImg, (w, h))
+    
+    # uNew = np.matmul(eigen, (newImg - mean_phi(imgfaces)))
+    # nilaiI = 0
+    # for i in range(len(imgfaces)):
+    #     dist = np.linalg.norm(uNew - eigen_face(find_covariance(imgfaces), imgfaces, i))
+    #     if i == 0:
+    #         temp = dist
+    #     elif (dist < temp):
+    #         temp = dist
+    #         nilaiI = i
+    # print("Euclidean distance :", temp)
+    # data = im.fromarray(eigen_face(find_covariance_method_concatenate(imgfaces), imgfaces))
+    t.tac()
+    # data.show()
 
 if __name__ == '__main__':
     __main__()
