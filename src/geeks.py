252d34a4f8c16980bb95cbf5742ee19391463e65
energizedNachos <16521108@mahasiswa.itb.ac.id>
2022-11-16 00:20:37 +0700

    feat: mungkin ada hope :")

--- src/geeks.py
+++ src/geeks.py
@@ -15,11 +15,17 @@ def plot_portraits(images, titles, h, w, n_row, n_col):
         plt.xticks(())
         plt.yticks(())
 
+def euclideanDistance(cobaNyimpen1, cobaNyimpen2):
+    total = 0
+    for i in range (len(cobaNyimpen1)):
+        total += (cobaNyimpen1[i] - cobaNyimpen2[i])**2
+    return np.sqrt(total)
+
 def main():
     database = []
     matrixbaru = []
     
-    ip.read_training_data_set("Zendaya", database)
+    ip.read_training_data_set("Zoe Saldana", database)
     meandatabase = mean_database(database, mp.mean_phi(database))
     for item in meandatabase:
         matrixbaru.append(item.reshape(w*h, 1))
@@ -28,22 +34,47 @@ def main():
     matrixA = concate(matrixbaru)
     eigenval, eigenvec = np.linalg.eig(matKov)
     eigenFaceArray = []
-    for i in range(len(matrixbaru)):
+    
+    K = 10
+    for i in range(K):
         eigenFaceArray.append(np.matmul(matrixA, eigenvec[i]))
 
-    print(len(eigenFaceArray))
-    eigenface_titles = ["eigenface %d" % i for i in range(len(eigenFaceArray))]
-    plot_portraits(eigenFaceArray, eigenface_titles, h, w, 5, 4)
-    plt.show()
+    # eigenface_titles = ["eigenface %d" % i for i in range(len(eigenFaceArray))]
+    # plot_portraits(eigenFaceArray, eigenface_titles, h, w, 5, 2)
+    # plt.show()
 
-    # eigenVal, eigenVec  = np.linalg.eig(matKov)
-    # idx = eigenVal.argsort()[::-1]
-    # eigenVal = eigenVal[idx]
-    # eigenvec = eigenVec[:,idx]
+    #"Database koefisien2 semua wajah"
+    nilaiX = []
+    for i in range(len(database)):
+        cobaNyimpen = []
+        for j in range(K):
+            cobaNyimpen.append(np.dot(meandatabase[i].reshape(w*h, 1).T, eigenFaceArray[j]))
+        nilaiX.append(cobaNyimpen)
+
+    testImage = ip.read_image("testing.jpg")
+    testImage = abs(testImage-mp.mean_phi(database))
+    nilai2 = []
+    for i in range(len(database)):
+        cobaNyimpen = []
+        for j in range(K):
+            cobaNyimpen.append(np.dot(testImage.reshape(w*h, 1).T, eigenFaceArray[j]))
+        nilai2.append(cobaNyimpen)
     
-    # eigenFace = np.matmul(matKov, )
-    # ip.print_img(eigenFace)
-    # eigFace1 = eigenFace[0:255, 0:255]
+    min = euclideanDistance(nilaiX[0], nilai2[0])
+    index = 0
+    for i in range(len(nilaiX)):
+        dist = euclideanDistance(nilaiX[i], nilai2[0])
+        print(dist)
+        if dist < min:
+            min = dist
+            index = i
+    print(min)
+    print(index)
+    ip.print_img(database[index])
+    # print(len(eigenFaceArray))
+    # eigenface_titles = ["eigenface %d" % i for i in range(len(eigenFaceArray))]
+    # plot_portraits(eigenFaceArray, eigenface_titles, h, w, 5, 4)
+    # plt.show()
     
 
 def concate(database):
@@ -61,7 +92,7 @@ def matCov(meandatabase):
 def mean_database(database, mean):
     res = []
     for m in database:
-        res.append(abs(m-mean))
+        res.append(abs(m-mean)*1/10000000)
     return res
 
 main()
