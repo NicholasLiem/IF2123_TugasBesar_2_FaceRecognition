cc6376f904b726ae5e3a4117d6ba05da90c80fbc
energizedNachos <16521108@mahasiswa.itb.ac.id>
2022-11-21 21:34:54 +0700

    fix: benerin read

--- src/imgparsing.py
+++ src/imgparsing.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 w, h = 256, 256
 
 # Fungsi yang menyimpan image sebagai array dan menampungkan pada sebuah database #
-def read_training_data_set(nama, dataset):
+def read_training_data_set(nama, dataset, datalabel):
     path = os.getcwd()
     path = path + "\\test\\database\\" + nama
     dir_list = os.listdir(path)
@@ -13,7 +13,7 @@ def read_training_data_set(nama, dataset):
         img = cv2.imread(path + '\\' + pic, cv2.IMREAD_GRAYSCALE)
         img = cv2.resize(img, (w, h))
         dataset.append(img)
-        # datalabel.append(pic)
+        datalabel.append(pic)
     print("Dataset has been loaded.")
 
 # Fungsi yang membaca image ke array dgn membaca file #
